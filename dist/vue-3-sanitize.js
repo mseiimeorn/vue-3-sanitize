3bf871ac74655b5b1cb81aa9122afd6b7a08ffa6
Simon Meier <s10.m313@gmail.com>
2022-02-17 16:46:09 +0100

    adding use function and export

--- dist/vue-3-sanitize.js
+++ dist/vue-3-sanitize.js
@@ -1,22 +1,36 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global['vue-3-sanitize'] = factory());
-}(this, (function () { 'use strict';
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["vue-3-sanitize"] = factory());
+})(this, (function () { 'use strict';
 
     var sanitizeHtml = require("sanitize-html");
 
+    var vue = require('vue');
+
+    var vue3SanitizeKey = Symbol('vue-3-sanitize');
     var Vue3Sanitize = {
       install: function install(app, options) {
-        app.config.globalProperties.$sanitize = function (dirty) {
+        var sanitize = function sanitize(dirty) {
           var opts = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
           return sanitizeHtml(dirty, opts || options);
         };
+
+        app.config.globalProperties.$sanitize = sanitize;
+        app.provide(vue3SanitizeKey, {
+          sanitize: sanitize
+        });
       },
       defaults: sanitizeHtml.defaults
     };
 
+    var useVue3Sanitize = function useVue3Sanitize() {
+      vue.inject(vue3SanitizeKey);
+    };
+
+    exports.useVue3Sanitize = useVue3Sanitize;
+
     return Vue3Sanitize;
 
-})));
+}));
 //# sourceMappingURL=vue-3-sanitize.js.map
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const sanitizeHtml = require("sanitize-html");
+const vue = require('vue');
+
+const vue3SanitizeKey = Symbol('vue-3-sanitize');
 
 const Vue3Sanitize = {
     install: (app, options) => {
-        app.config.globalProperties.$sanitize = (dirty, opts = null) =>
+        const sanitize = (dirty, opts = null) => 
             sanitizeHtml(dirty, opts || options);
+
+        app.config.globalProperties.$sanitize = sanitize;
+        
+        app.provide(vue3SanitizeKey, { sanitize })
     },
 
     defaults: sanitizeHtml.defaults,
 };
 
+const useVue3Sanitize = () => {
+    vue.inject(vue3SanitizeKey);
+};
+
+exports.useVue3Sanitize = useVue3Sanitize;
 export default Vue3Sanitize;
